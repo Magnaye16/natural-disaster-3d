e491eea775d2269cd1c488c5d09759ef1cc53797
c0mplex <snakebigbosslel@gmail.com>
2025-09-03 22:38:26 +0800

    Finished setting up basic movement

---
+++ scenes/PrototypeTestWorld/game.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+#@onready var camera: Camera3D = $Camera/WorldCam
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+#	print(camera.get_global_transform().basis.z)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
