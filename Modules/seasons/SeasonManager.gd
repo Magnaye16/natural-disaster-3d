64a2a16380ac05ecf9a5c4d1b459b46435de9913
erenjhun <enerjhunrelon@gmail.com>
2025-09-10 23:36:43 +0800

    feat: enhance Season and SeasonManager classes with weather rehydration and improved structure

--- Modules/seasons/SeasonManager.gd
+++ Modules/seasons/SeasonManager.gd
@@ -1,18 +1,19 @@
+@tool
 extends Node
 class_name SeasonManager
 
 @export var seasons: Array[Season] = []
 
 
-signal day_advanced(day:int)
-signal season_advanced(season:Season)
-signal year_advanced(year:int)
+signal day_advanced(day: int)
+signal season_advanced(season: Season)
+signal year_advanced(year: int)
 
 var _current_season: Season:
-	get=get_current_season
+	get = get_current_season
 
 
-var starting_season_idx:int
+var starting_season_idx: int
 var season_idx: int
 
 var current_day: int = 1
@@ -20,7 +21,13 @@ var current_year: int = 1
 var total_days: int = 1
 
 
-func reset()->void:
+func _ready() -> void:
+	for season in seasons:
+		season.rehydrate_posible_weathers()
+		print(seasons)
+
+
+func reset() -> void:
 	_current_season = null
 	starting_season_idx = 0
 
@@ -45,12 +52,10 @@ func pick_starting_season(idx: int):
 	season_advanced.emit(get_current_season())
 	print("startting season set to:%s"%get_current_season().name)
 
-func _ready():
-	pass
 
 
-func advance_day(days:int = 1):
 
+func advance_day(days: int = 1):
 	total_days += days
 	current_day += days
 
@@ -64,18 +69,12 @@ func advance_day(days:int = 1):
 	day_advanced.emit(current_day)
 
 func advance_season():
-	print("season id:%s seasons size:%s "%[season_idx,seasons.size()-1])
 	season_idx += 1
-
-	print("season id:%s  starting season:%s total_days:%s"%[season_idx,starting_season_idx,total_days])
 	season_idx = season_idx % (seasons.size())
-
 	season_advanced.emit(get_current_season())
-
 	#checks if this season is same as the starting season
-	print("season id:%s "%[season_idx])
 	if season_idx == starting_season_idx and total_days > 1:
-		current_year +=1
+		current_year += 1
 		year_advanced.emit(current_year)
 
 
@@ -88,3 +87,13 @@ func get_current_season() -> Season:
 # Helper function to get current season name
 func get_season_name() -> String:
 	return _current_season.name if _current_season else "Unknown"
+
+
+func season_class_from_string(season_class_name:String)->Season:
+	var resource_path = "res://Modules/seasons/types/%s.gd" % season_class_name
+	var season_script:Season = load(resource_path)
+	if season_script:
+		return season_script
+	else:
+		push_error("Season script not found for: %s" % season_class_name)
+		return null
