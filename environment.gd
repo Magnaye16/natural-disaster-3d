22bd53dd81543a893566666a5228ae8ea0fef20b
erenjhun <enerjhunrelon@gmail.com>
2025-09-12 00:39:42 +0800

    refactor: update game_manager references and remove autoload configuration for Season_manager

--- environment.gd
+++ environment.gd
@@ -10,17 +10,19 @@ var game_manager:GameManager = GameManager.new()
 
 
 func _ready():
-	Season_manager.day_advanced.connect(
+	game_manager.init()
+
+	game_manager.season_manager.day_advanced.connect(
 		func(day): day_label.text = "%s"%day
 )
 
-	Season_manager.season_advanced.connect(
+	game_manager.season_manager.season_advanced.connect(
 		func(season:Season):
 		season_label.text = "%s"%season.name
 		season_texture.texture = season.sprite
 )
 
-	Season_manager.year_advanced.connect(
+	game_manager.season_manager.year_advanced.connect(
 		func(year): year_label.text = "%s"%year
 )
 	game_manager.weather_manager.weather_advanced.connect(
@@ -33,43 +35,38 @@ func _ready():
 
 func advance_day():
 	game_manager.advance_day()
-	game_manager.advance_weather()
+
 
 func plus10_day() -> void:
 	game_manager.advance_day()
 	game_manager.advance_weather()
 
 func set_starting_cool():
-	Season_manager.reset()
-	Season_manager.pick_starting_season(0)
-	game_manager.advance_weather()
+	game_manager.set_starting_season("ColdDry")
 
 
 func set_starting_hot():
-	Season_manager.reset()
-	Season_manager.pick_starting_season(1)
-	game_manager.advance_weather()
+	game_manager.set_starting_season("HotDry")
+
 
 
 func set_starting_rainy():
-	Season_manager.reset()
-	Season_manager.pick_starting_season(2)
-	game_manager.advance_weather()
+	game_manager.set_starting_season("Rainy")
+
 
 
 func set_starting_typhoon():
-	Season_manager.reset()
-	Season_manager.pick_starting_season(3)
-	game_manager.advance_weather()
+	game_manager.set_starting_season("Typhoon")
+
 
 
 func set_labels():
-	if Season_manager.get_current_season() == null: return
+	if game_manager.get_current_season() == null: return
 
-	season_texture.texture = Season_manager.get_current_season().sprite
-	season_label.text = "%s"%Season_manager.get_current_season().name
-	day_label.text = "%s"%Season_manager.current_day
-	year_label.text = "%s"%Season_manager.current_year
+	season_texture.texture = game_manager.get_current_season().sprite
+	season_label.text = "%s"%game_manager.get_current_season().name
+	day_label.text = "%s"%game_manager.season_manager.current_day
+	year_label.text = "%s"%game_manager.season_manager.current_year
 
 
 func new_weather() -> void:
