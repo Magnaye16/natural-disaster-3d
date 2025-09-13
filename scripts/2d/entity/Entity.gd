e2633189c3618ebb93496daeb0448f49110616aa
c0mplex <snakebigbosslel@gmail.com>
2025-09-14 00:01:49 +0800

    removed move, now a private function
    type annotations for instance properties are now notated my way XD

--- scripts/2d/entity/Entity.gd
+++ scripts/2d/entity/Entity.gd
@@ -1,12 +1,7 @@
 class_name Entity extends CharacterBody2D
 
-var inventory:Inventory = null
+var inventory : Inventory = null
 
-
-func set_movement_direction(direction: Vector2) -> void:
-	pass
-
-func move(delta: float) -> void:
 func set_movement_direction(_direction: Vector2) -> void:
 	pass
 	
