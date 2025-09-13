7465c936e7d157a6de9c359485ad496e7c31af1d
c0mplex <snakebigbosslel@gmail.com>
2025-09-13 23:55:46 +0800

    Removed move as its now a private function

--- scripts/2d/entity/Entity.gd
+++ scripts/2d/entity/Entity.gd
@@ -1,7 +1,8 @@
-﻿class_name Entity extends CharacterBody2D
+class_name Entity extends CharacterBody2D
 
 func set_movement_direction(direction: Vector2) -> void:
 	pass
 
 func move(delta: float) -> void:
 	pass
+	
