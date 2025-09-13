b743868cf943ed485ad9ec0f00b76e3041819867
c0mplex <snakebigbosslel@gmail.com>
2025-09-13 23:54:50 +0800

    Player implementation of EntityController

---
+++ scripts/Controller/PlayerController.gd
@@ -0,0 +1,8 @@
+﻿class_name PlayerController extends EntityController
+
+func _physics_process(_delta: float) -> void:
+	var direction : Vector2 = Input.get_vector(
+						MoveDirection.MOVE_LEFT, MoveDirection.MOVE_RIGHT,
+						MoveDirection.MOVE_UP, MoveDirection.MOVE_DOWN,)
+	print(direction)
+	move_command.execute(entity, direction)
