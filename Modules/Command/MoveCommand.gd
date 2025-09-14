d09e1aa6fa0343b076ebbc2a9e9b673cb839c722
erenjhun <enerjhunrelon@gmail.com>
2025-09-14 17:34:22 +0800

    format:make the reciever type into correct type 'MovementComponent

--- Modules/Command/MoveCommand.gd
+++ Modules/Command/MoveCommand.gd
@@ -5,7 +5,7 @@ var reciever
 var dir:Vector2
 
 
-func _init(reciever_:MoveCommand,dir_:Vector2)->void:
+func _init(reciever_:MovementComponent,dir_:Vector2)->void:
 	reciever = reciever_
 	dir = dir_
 
