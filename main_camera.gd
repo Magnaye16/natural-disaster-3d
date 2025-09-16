a730ebfc07b3af9e3bec50f0d6dd0ea600294fae
erenjhun <enerjhunrelon@gmail.com>
2025-09-17 00:12:47 +0800

    feat: added seperate camera with screen shake and smooth follow

---
+++ main_camera.gd
@@ -0,0 +1,43 @@
+extends Camera2D
+
+@export var follow_speed: float = 5.0
+
+var target: Player
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_key_pressed(KEY_SPACE):
+		shake(3, 1, 200)
+
+func _ready() -> void:
+	await get_tree().process_frame
+	target = get_tree().get_first_node_in_group("player")
+
+
+var _shake_offset: Vector2 = Vector2.ZERO
+func _process(delta: float) -> void:
+	if not is_instance_valid(Player):return
+	var base_pos  = global_position.lerp(target.global_position, follow_speed * delta)
+	global_position = base_pos + _shake_offset
+
+
+var _shake_tween: Tween
+
+func shake(intensity: float = 10.0, duration: float = 1.0, frequency: float = 8.0) -> void:
+	if _shake_tween and _shake_tween.is_running():
+		_shake_tween.kill()
+
+	_shake_tween = create_tween()
+	_shake_tween.set_trans(Tween.TRANS_SINE)
+	_shake_tween.set_ease(Tween.EASE_IN_OUT)
+
+	var step = duration / frequency
+	for i in range(int(frequency)):
+		var offset = Vector2(
+			randf_range(-intensity, intensity),
+			randf_range(-intensity, intensity)
+	)
+		_shake_tween.tween_property(self, "_shake_offset", offset, step)
+
+	# Reset shake back to zero
+	_shake_tween.tween_property(self, "_shake_offset", Vector2.ZERO, step)
