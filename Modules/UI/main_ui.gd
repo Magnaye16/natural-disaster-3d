e05c6b9b5fc850068c5268eb69486e4cf46ecd2c
erenjhun <enerjhunrelon@gmail.com>
2025-09-17 00:14:40 +0800

    refactor:auto hide tooltips via code

--- Modules/UI/main_ui.gd
+++ Modules/UI/main_ui.gd
@@ -7,18 +7,15 @@ extends CanvasLayer
 
 func _ready() -> void:
 	await get_tree().process_frame
-
+	interactable_tooltip.hide()
 	var player:Player = get_tree().get_first_node_in_group("player") as Player
-
-
 	player.interactable_found.connect(
 		interactable_tooltip.show
 	)
-
 	player.interactable_lost.connect(
 		interactable_tooltip.hide
 	)
-
 	player.inventory_requested.connect(
 		func():inventory_ui.show() if not inventory_ui.visible else inventory_ui.hide()
 	)
+	show()
