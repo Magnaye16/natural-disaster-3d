1aee3690c0a0ee1f542721833c89a2cec53210f1
Sleepy <Roijohn.magnaye@gmail.com>
2025-09-19 21:28:16 +0800

    feat: integrate status multiplier for movement and health regeneration

--- Modules/Components/MovementComponent.gd
+++ Modules/Components/MovementComponent.gd
@@ -5,6 +5,7 @@ extends Node
 @export var speed : float = 400.0
 @export var acceleration : float = 7000.0
 @export var friction : float = 7000
+@export var status_multiplier:StatusContainer
 
 
 var move_direction : Vector2 = Vector2.ZERO # Save the move_direction so when we use cmd pattern we just change this
@@ -19,7 +20,15 @@ func _physics_process(delta: float) -> void:
 
 func _move(delta: float) -> void:
 	if move_direction:
-		velocity = velocity.lerp(move_direction * speed, 1 - exp(-acceleration * delta))
+		#var product_speed:float = status_multiplier.status_array.reduce(
+			#func (accum,status:Status):return status.apply_multiplier(speed),speed
+			#)
+		var product_speed:float = speed
+		
+		for stat in status_multiplier.status_array:
+			product_speed = stat.apply_multiplier(product_speed)
+		speed = max(0.1, speed)
+		velocity = velocity.lerp(move_direction * product_speed, 1 - exp(-acceleration * delta))
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 
--- Modules/Inventory/Component/HealthComponent/HealthComponent.gd
+++ Modules/Inventory/Component/HealthComponent/HealthComponent.gd
@@ -3,12 +3,33 @@ class_name HealthComponent
 
 var value:int = 1
 @export var max_value:int = 3
+@export var natural_regen_time:float = 5
+var natural_regen:int = 1
+var ticks:float = 1
+@export var status_multiplier:StatusContainer
+const MIN_HEALTH_REGEN:int = 1
 
 signal updated(HP)
 signal depleted
 
 func _ready() -> void:
 	set_HP(max_value)
+
+func _process(delta: float) -> void:
+	if value < max_value: 
+		ticks += delta
+	
+	if ticks > natural_regen_time:
+
+		natural_regen = status_multiplier.compute_value(natural_regen)
+		print(natural_regen)
+		natural_regen = max(MIN_HEALTH_REGEN, natural_regen)
+		
+		
+		set_HP(value + natural_regen)
+		ticks = 0
+
+
 
 func set_HP(_value:int):
 	value = _value
