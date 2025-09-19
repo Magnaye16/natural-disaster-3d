63954d656b1cdaafa9581b8c1adea1090536f35d
Sleepy <Roijohn.magnaye@gmail.com>
2025-09-19 21:29:08 +0800

    feat: add apply_status method to Entity class for status effect application

--- Modules/Entities/entity/Entity.gd
+++ Modules/Entities/entity/Entity.gd
@@ -1,5 +1,10 @@
 class_name Entity extends CharacterBody2D
 
 
+
 func set_movement_direction(_direction: Vector2) -> void:
 	pass
+
+
+func apply_status(status: Status) -> void:
+	(%StatusManager as StatusManager).apply_status(status)
