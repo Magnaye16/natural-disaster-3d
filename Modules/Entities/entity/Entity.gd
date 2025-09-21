51a03be96c90aa9405934edcc33dddb38d4e0659
erenjhun <enerjhunrelon@gmail.com>
2025-09-21 20:01:12 +0800

    refactor: removed  set movement function ( which is now present in movement comp)

--- Modules/Entities/entity/Entity.gd
+++ Modules/Entities/entity/Entity.gd
@@ -2,9 +2,5 @@ class_name Entity extends CharacterBody2D
 
 
 
-func set_movement_direction(_direction: Vector2) -> void:
-	pass
-
-
 func apply_status(status: Status) -> void:
 	(%StatusManager as StatusManager).apply_status(status)
