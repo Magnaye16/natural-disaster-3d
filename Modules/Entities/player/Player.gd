6962389474ac61c21bf71fc9780f236cc18801ea
erenjhun <enerjhunrelon@gmail.com>
2025-09-21 20:36:49 +0800

    refactor: rebuild

--- Modules/Entities/player/Player.gd
+++ Modules/Entities/player/Player.gd
@@ -8,30 +8,18 @@ var speed = 150
 @export var healthComponent:HealthComponent
 
 
-
 signal interactable_found
 signal interactable_lost
-
 
 signal inventory_requested
 
 
-#func get_Input():
-	#
-	#velocity = input_Direction * speed
-
 func _physics_process(_delta):
-	#get_Input()
-	#move_and_slide()
 	update_Animation()
 
 func _unhandled_input(event: InputEvent) -> void:
-
-
 	if Input.is_action_just_pressed("hit_btn"):
-		print("plapalpalpal")
 		($HealthComponent as HealthComponent).apply_DMG(1)
-
 
 	if event.is_action_pressed("ui_inventory"):
 		inventory_requested.emit()
