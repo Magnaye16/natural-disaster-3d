4391240c77b1b3dde6baf69dfcfa36cb300c01c9
erenjhun <enerjhunrelon@gmail.com>
2025-09-21 18:27:26 +0800

    refactor: minor adjust

--- Modules/Components/EnvironmentComponent/RainComponent.gd
+++ Modules/Components/EnvironmentComponent/RainComponent.gd
@@ -1,11 +1,30 @@
 extends Marker2D
 
 
-# Called when the node enters the scene tree for the first time.
+@export var camera:GlobalCamera:
+	set(val):
+		if is_instance_valid(val):
+			set_process(true)
+		else:
+			set_process(false)
+		camera = val
+@onready var rain: GPUParticles2D = $rain
+
 func _ready() -> void:
-	pass # Replace with function body.
+	camera=camera
+
+func enable():
+	rain.set_emitting(true)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func disable():
+	rain.set_emitting(false)
+
+##
+#sfa
+##
+func intensity(val:int):
+	rain.amount_ratio = val
+
+func _process(_delta: float) -> void:
+	global_position = camera.global_position
