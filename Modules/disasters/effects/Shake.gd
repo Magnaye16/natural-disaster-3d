3720cfb8879cabadc7a1a98db4da8612d742adbd
erenjhun <enerjhunrelon@gmail.com>
2025-09-22 23:05:45 +0800

    refactor: added export variables feat:earthquake

--- Modules/disasters/effects/Shake.gd
+++ Modules/disasters/effects/Shake.gd
@@ -1,5 +1,17 @@
 extends DisasterEffect
+class_name ShakeEffect
 
+@export var INTENSITY:int = 1
+
+var intensity:int:
+	get:return INTENSITY * 2
+
+@export var duration:float = 5
+var frequency:float = 100:
+	get:return intensity * 80
 
 func  _enter():
-	game_manager.get_tree().get_first_node_in_group("global_camera").shake()
+	(game_manager
+	.get_tree()\
+	.get_first_node_in_group("global_camera") as GlobalCamera)\
+	.shake(intensity,duration,frequency)
