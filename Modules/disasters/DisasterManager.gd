6fad971b6714fd431a831db63a6073f6ef7bb3e8
erenjhun <enerjhunrelon@gmail.com>
2025-09-22 23:02:45 +0800

    refactor: now alerts detectors globally

--- Modules/disasters/DisasterManager.gd
+++ Modules/disasters/DisasterManager.gd
@@ -4,9 +4,15 @@ extends Node
 @export var disasters: Array[Disaster] = []
 @export var curr_disaster:Disaster
 
+
+
 func try_generate_disaster(game_manager: GameManager) -> void:
 	for disaster in posible_disasters(game_manager):
 			disaster.trigger(game_manager)
+			alert_detectors(disaster)
+
+func alert_detectors(disaster:Disaster):
+		get_tree().call_group("detector","_disaster_detected",disaster)
 
 func posible_disasters(game_manager)->Array[Disaster]:
 	return disasters.filter(func (dis:Disaster):return dis.can_trigger(game_manager))
