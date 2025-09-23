e0143f3e143c6dabcafea79e9ffefe80f6fa9ad2
erenjhun <enerjhunrelon@gmail.com>
2025-09-23 20:55:25 +0800

    refactor:remove print statements

--- Modules/Components/EnvironmentComponent/Fire/FireComponent.gd
+++ Modules/Components/EnvironmentComponent/Fire/FireComponent.gd
@@ -8,29 +8,22 @@ var ticks:float = 0
 var duration: float = 0.5
 
 
-
-
 func _process(_delta: float) -> void:
 	#if Damage < Health.max_value:
 	if status_container.status_array.is_empty():
 		hide()
 	else:
-		print(status_container.status_array.get(0).Duration)
 		show()
 	ticks += _delta
-	
+
 	if ticks > duration:
 		var Product_Damage
 		if status_container:
 			Product_Damage = status_container.compute_value(Damage)
-			
-		
+
+
 		Health.apply_DMG(Product_Damage)
 		ticks = 0
 
 func apply_damage_fire_status(status:Status):
-	status_container.check_stackable_status(status)
-	print(status)
-	
-	
-	
+	status_container.add_status(status)
