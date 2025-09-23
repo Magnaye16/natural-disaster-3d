caa794766c09c054be0e466d6507a7143a906003
erenjhun <enerjhunrelon@gmail.com>
2025-09-23 20:56:40 +0800

    feat:added remove status function

--- Modules/Entities/entity/Entity.gd
+++ Modules/Entities/entity/Entity.gd
@@ -6,3 +6,8 @@ class_name Entity extends CharacterBody2D
 func apply_status(status: Status) -> void:
 	(component_manager.get_component(StatusManagerComponent)
 	as StatusManagerComponent).apply_status(status)
+
+
+func remove_status(status: Status) -> void:
+	(component_manager.get_component(StatusManagerComponent)
+	as StatusManagerComponent).remove_status(status)
