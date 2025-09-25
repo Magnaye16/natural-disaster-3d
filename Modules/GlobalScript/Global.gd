f29cddc693cb6184ebf50496e997622dfa047946
erenjhun <enerjhunrelon@gmail.com>
2025-09-26 00:42:59 +0800

    feat: function to query the game_manager

--- Modules/GlobalScript/Global.gd
+++ Modules/GlobalScript/Global.gd
@@ -2,3 +2,6 @@ extends Node
 
 func get_player()->Player:
 	return get_tree().get_first_node_in_group("player")
+
+func get_game_manager()->GameManager:
+	return get_node("../Main_game/GameManager")
