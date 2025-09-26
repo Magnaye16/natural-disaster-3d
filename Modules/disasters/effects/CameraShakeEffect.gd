2aa3171746b992481b720add54c307859ba730dc
c0mplex <snakebigbosslel@gmail.com>
2025-09-26 17:59:16 +0800

    refactored camera shaking to a separate effect

---
+++ Modules/disasters/effects/CameraShakeEffect.gd
@@ -0,0 +1,11 @@
+﻿extends ShakeEffect
+class_name CameraShakeEffect
+
+var frequency:float = 100:
+	get:return intensity * 80
+
+func _enter() -> void:
+	(game_manager
+	.get_tree()\
+	.get_first_node_in_group("global_camera") as GlobalCamera)\
+	.shake(intensity,duration,frequency)
--- Modules/disasters/effects/Shake.gd
+++ Modules/disasters/effects/Shake.gd
@@ -7,14 +7,8 @@ var intensity:int:
 	get:return INTENSITY * 2
 
 @export var duration:float = 5
-var frequency:float = 100:
-	get:return intensity * 80
 
-func  _enter():
-	(game_manager
-	.get_tree()\
-	.get_first_node_in_group("global_camera") as GlobalCamera)\
-	.shake(intensity,duration,frequency)
 
+func _enter() -> void:
 	var player:Player = Global.get_player()
 	player.controller_manager.set_controller_by_class(ShakingPlayerController)
