c31dcf051d7e27a9a7f73303210af3a5f6e25319
erenjhun <enerjhunrelon@gmail.com>
2025-09-27 23:34:41 +0800

    refactor: decouple controller manager and controller -made process,input,setup accept entity argument from the manager -change_state now just requests to manager for an actual change state

--- Modules/Command/Controller/ControllerManager.gd
+++ Modules/Command/Controller/ControllerManager.gd
@@ -3,17 +3,29 @@ extends Node
 
 var active_controller: BaseController = null
 @export var controllers: Dictionary[String,BaseController]
-@export var manager_owner:Entity
+var manager_owner:Entity
 
 func _ready() -> void:
 	set_process(false)
-	_cache_children()
 	manager_owner = get_parent()
+
+	_cache_children()
+
+
+
+func ready_controller(controller:BaseController)->void:
+	controller._all_ready()
+	var initial_state:BaseController.State =controller._setup_states(manager_owner)
+	controller.curr_state = initial_state
+
+	assert(controller.curr_state,"_setup_states must return a current state")
+	controller.curr_state._enter.call(manager_owner)
 
 func _cache_children():
 	for child in get_children():
 		if child is BaseController:
 			_cache_controller(child)
+
 
 	if controllers.size() > 0:
 		active_controller = get_child(0)
@@ -27,14 +39,16 @@ func set_controller_by_class(ctrl_class: GDScript) -> void:
 
 	if not ctrl:
 		ctrl = ctrl_class.new()
-
-	add_controller(ctrl)
-	set_deferred("active_controller",ctrl)
+		add_controller(ctrl)
+	active_controller = ctrl
+	active_controller._activate(manager_owner)
 
 
 func add_controller(ctrl:BaseController)->void:
 	_cache_controller(ctrl)
 	add_child(ctrl)
+	ready_controller(active_controller)
+
 
 
 func set_controller(ctrl: BaseController) -> void:
@@ -42,16 +56,36 @@ func set_controller(ctrl: BaseController) -> void:
 	set_deferred("active_controller",ctrl)
 
 
-func _cache_controller(cotroller:BaseController)->void:
-	controllers.set(cotroller.get_script().get_global_name(),cotroller)
+func _cache_controller(controller:BaseController)->void:
+	controllers.set(controller.get_script().get_global_name(),controller)
+	controller.requested_change_state.connect(
+		func(new_state):
+			if not controller.get_script() == active_controller.get_script():return
+			change_state(new_state)
+	)
+	ready_controller(controller)
+
+
+
+func change_state(new_state:BaseController.State):
+	print(active_controller.get_script().get_global_name(),"  request----->",new_state.name)
+	active_controller.prev_state = active_controller.curr_state
+	active_controller.curr_state = new_state
+	print(active_controller.prev_state.name," -> ",active_controller.curr_state.name)
+	active_controller.prev_state._exit.call(manager_owner)
+	active_controller.curr_state._enter.call(manager_owner)
 
 func _process(_delta: float) -> void:
 	await get_tree().process_frame
 	if active_controller == null:
 		set_process(false)
 		return
-	active_controller.__process()
+
+	active_controller.__process(manager_owner)
+	$"../Label".text = "%s    %s"%[active_controller.name,active_controller.curr_state.name]
+
 
 func _unhandled_key_input(_event: InputEvent) -> void:
 	if active_controller == null:return
-	active_controller.__input()
+	print(active_controller.curr_state.name)
+	active_controller.__input(manager_owner)
--- Modules/Command/Controller/baseController.gd
+++ Modules/Command/Controller/baseController.gd
@@ -9,10 +9,10 @@ var prev_state:State
 
 class State:
 	@warning_ignore_start("unused_private_class_variable")
-	var _enter:Callable = func():pass
-	var _exit:Callable = func():pass
-	var _input:Callable = func():pass
-	var _process:Callable = func():pass
+	var _enter:Callable = func(_entity):pass
+	var _exit:Callable = func(_entity):pass
+	var _input:Callable = func(_entity):pass
+	var _process:Callable = func(_entity):pass
 	var name:String
 	var active:bool:
 		get():return manager.curr_state == self
@@ -32,42 +32,29 @@ class State:
 				_cb.call()
 		)
 
-
-func get_manager()->ControllerManager:
-
-	return get_parent()
-
-func _notification(what: int) -> void:
-	if what == NOTIFICATION_READY:
-		await  get_tree().process_frame
-		_all_ready()
-		curr_state = _setup_states()
-		if curr_state:curr_state._enter.call()
-
+func _activate(player:Player):
+	curr_state._enter.call(player)
 
 func _all_ready()->void:
 	pass
 
 ##The base class calls this and sets the curr_state to the state it returns
-func _setup_states()->State:
+func _setup_states(_entity)->State:
 	return null
 
 
+signal requested_change_state
 func change_state(new_state:State):
-	prev_state = curr_state
-	curr_state = new_state
-	print(prev_state.name," -> ",curr_state.name)
-	prev_state._exit.call()
-	curr_state._enter.call()
+	requested_change_state.emit(new_state)
 
 ## don't forget to call super.__process() when overriding this[br]
 ##calls curr_state_process
-func __process()->void:
+func __process(manager_owner:Node)->void:
 	if not curr_state:return
-	curr_state._process.call()
+	curr_state._process.call(manager_owner)
 
 ## don't forget to call super.__input() when overriding this[br]
 ##calls curr_state._input
-func __input()->void:
+func __input(manager_owner:Node)->void:
 	if not curr_state:return
-	curr_state._input.call()
+	curr_state._input.call(manager_owner)
