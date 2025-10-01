dfb7baa5562a8af878effac4ec78044bef711426
Eren <enerjhunrelon@gmail.com>
2025-10-01 16:56:27 +0800

    goodhist

--- Modules/Objects/furnitures/table.gd
+++ Modules/Objects/furnitures/table.gd
@@ -7,13 +7,6 @@ func _on_interactable_component_interacted(_entity: Entity) -> void:
 	
 	var sprite:SpriteComponent =  entity.component_manager.get_component(SpriteComponent)
 	sprite.play(&"cover")
-	
-	var dople=sprite.duplicate()
-	
-	add_child(dople)
-	
-	var scale_off:Vector2 = Vector2(4	,1)
-	#entity.global_position = global_position 
 
-	dople.global_position = global_position + scale_off
+	entity.global_position = global_position
 	
