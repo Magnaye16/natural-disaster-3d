32d15cf49ec872a09f0d5e00114a0cc67191dc8d
Sleepy <Roijohn.magnaye@gmail.com>
2025-10-02 21:44:02 +0800

    feat: add audio stream player and loading screen music

--- scenes/Screens/Screen.gd
+++ scenes/Screens/Screen.gd
@@ -2,6 +2,7 @@ extends Node2D
 @onready var progress_bar: ProgressBar = $"Loading Screen/Panel/ProgressBar"
 @onready var loading_screen: Node2D = $"Loading Screen"
 @onready var start_screen: Node2D = $"Start Screen"
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 var value: float
 
@@ -10,12 +11,14 @@ func  _ready() -> void:
 
 func _process(delta: float) -> void:
 	if progress_bar.value < progress_bar.max_value:
-		progress_bar.value += 15 * delta
+		progress_bar.value += 10 * delta
 	else:
+		#audio_stream_player.play()
 		loading_screen.hide()
 		start_screen.show()
 	
 
 
 func _on_button_pressed() -> void:
+	audio_stream_player.stop()
 	get_tree().change_scene_to_file("res://Main_game.tscn")
