44490e46b51169832668e35d2da06d9880838175
erenjhun <enerjhunrelon@gmail.com>
2025-10-02 23:56:43 +0800

    feat: added screen animations

--- scenes/Screens/Screen.gd
+++ scenes/Screens/Screen.gd
@@ -1,7 +1,7 @@
-extends Node2D
-@onready var progress_bar: ProgressBar = $"Loading Screen/Panel/ProgressBar"
-@onready var loading_screen: Node2D = $"Loading Screen"
-@onready var start_screen: Node2D = $"Start Screen"
+extends Control
+@onready var progress_bar: ProgressBar = $loading/ProgressBar
+@onready var loading_screen: Panel =$loading
+@onready var start_screen: Panel = $start
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 var value: float
@@ -9,16 +9,17 @@ var value: float
 func  _ready() -> void:
 	progress_bar.value = 0
 
+
 func _process(delta: float) -> void:
 	if progress_bar.value < progress_bar.max_value:
-		progress_bar.value += 10 * delta
+		progress_bar.value += 100 * delta
 	else:
 		#audio_stream_player.play()
 		loading_screen.hide()
 		start_screen.show()
-	
-
 
 func _on_button_pressed() -> void:
-	audio_stream_player.stop()
+	$start/Button.hide()
+	$AnimationPlayer.play("start")
+	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file("res://Main_game.tscn")
