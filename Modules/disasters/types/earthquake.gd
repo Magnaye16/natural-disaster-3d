6e0dbebdcf4837683ca0ccf2e621f99b0e91c321
c0mplex <snakebigbosslel@gmail.com>
2025-10-05 22:27:28 +0800

    Aftershock property impl

--- Modules/disasters/types/earthquake.gd
+++ Modules/disasters/types/earthquake.gd
@@ -3,6 +3,13 @@ class_name EarthQuake
 
 
 
+@export_group("Aftershock_Res")
+@export_range(0.0, 1.0) var aftershock_chance: float = 1.0
+@export var aftershock_min_delay_seconds: float = 1.0
+@export var aftershock_max_delay_seconds: float = 60.0
+@export var aftershock_duration: float = 5.0
+@export var aftershock_effects: Array[DisasterEffect]
+@export var aftershock_exit_effects: Array[DisasterEffect]
 
 func _condition(_game_manager:GameManager)->bool:
 	return true
